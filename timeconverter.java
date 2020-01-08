66de869940956b3458e4a6e6110a1a95bf61a14b
Laura Alvarez <alvarez.bio26@gmail.com>
2020-01-07 21:42:17 -0500

    Update timeconverter.java

--- timeconverter.java
+++ timeconverter.java
@@ -7,7 +7,13 @@ public class timeconverter {
 		int seconds = hour * 3600;
 		
 		return seconds;	
-	}		
+	}	
+	
+	public static int convertSecToHours(int Sec) {
+		int hours = Sec/3600;
+		
+		return hours;	
+	}	
 	
 		
 	public static int convertMinToSeconds(int Min) {
@@ -16,18 +22,33 @@ public class timeconverter {
 		return seconds;
 	}
 	
+	public static int convertSecToMinutes(int sec) {
+		int minutes = sec/60;
+		
+		return minutes;
+	}
+	
 	public static int convertDaysToSeconds(int days) {
 		int seconds = days * 86400;
 		
 		return seconds;	
+	}
+	
+	public static int convertSecToDays(int sec) {
+		int days = sec/86400;
+		
+		return days;
 	}
 
 	
 	
 	public static void main(String[] args) {
 		System.out.println(convertHourToSeconds(9));
+		System.out.println(convertSecToHours(18000));
 		System.out.println(convertMinToSeconds(9));
+		System.out.println(convertSecToMinutes(18000);
 		System.out.println(convertDaysToSeconds(9));
+		System.out.println(convertSecToDays(20000000);		   		  
 
 	}
 
